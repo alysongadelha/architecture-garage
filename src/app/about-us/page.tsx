b619462c7cf0bea8650613d04f843283705a66be
Alyson Gadelha <alyson_sud@hotmail.com>
2023-06-11 19:53:28 +0100

    AG-0001: create mocked routes

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,9 @@
+const AboutUs = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-orange-200">
+      <h1 className="font-heading text-primaryColor text-2xl">About Us</h1>
+    </main>
+  );
+};
+
+export default AboutUs;
---
+++ src/models/Routes.ts
@@ -0,0 +1,4 @@
+export enum Routes {
+  CATALOG = "/shop",
+  ABOUT_US = "/about-us",
+}
