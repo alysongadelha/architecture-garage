bc4453c68275fc6be9bc729658ece342e9d073cf
Alyson Gadelha <alyson_sud@hotmail.com>
2024-06-14 21:57:59 +0100

    AG-0028: add revalidate of 1 minute to projects page

--- src/app/api/shop/projects/route.ts
+++ src/app/api/shop/projects/route.ts
@@ -1,6 +1,8 @@
 import { getCollection } from '@/firebase/firestore/getData'
 import { NextResponse } from 'next/server'
 
+export const revalidate = 60
+
 export async function GET() {
   const { result, responseError } = await getCollection('projects')
   return NextResponse.json({ result, responseError })
